fe8d7057788584608665f2ded44fc8777e2f7f50
ravindrapv <ravindra.ravi4849@gmail.com>
2021-12-08 23:18:31 +0530

    testing

---
+++ block-BJaafo/code/script.js
@@ -0,0 +1,12 @@
+function fullName(firstname,secondname){
+    return firstname + secondname;
+}
+let fullname = fullName(venkata,ravindra);
+
+if(fullname !== "venkata ravindra"){
+    console.log(error);
+}
+
+function totalTex(amount,texrate){
+    return amount+(amount*texrate);
+}
