56291864d655438cfd329a01844c879af5aad95c
ravindrapv <ravindra.ravi4849@gmail.com>
2021-12-09 15:09:55 +0530

    testing

--- block-BJaafo/code/script.js
+++ block-BJaafo/code/script.js
@@ -1,12 +1,19 @@
 function fullName(firstname,secondname){
     return firstname + secondname;
 }
-let fullname = fullName(venkata,ravindra);
+let fullname = fullName("venkata","ravindra");
 
-if(fullname !== "venkata ravindra"){
-    console.log(error);
+let expectedname = "venkataravindra";
+if(fullname !== expectedname){
+   throw Error(`${fullName} is not equel to ${expectedname}`);
 }
 
 function totalTex(amount,texrate){
     return amount+(amount*texrate);
+}
+let totaltex = totalTex(200,40);
+let expected = 400;
+
+if(totaltex !== expected){
+    throw Error(`${totalTex} is not equal to ${expected}`);
 }
